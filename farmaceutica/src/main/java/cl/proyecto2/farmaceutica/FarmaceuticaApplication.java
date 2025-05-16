0e707bff1059e7f15cf10c32029ddd379e27bd02
Joaco202 <joakocontreras06@gmail.com>
2025-05-15 21:15:12 -0400

    creacion del proyecto

---
+++ farmaceutica/src/main/java/cl/proyecto2/farmaceutica/FarmaceuticaApplication.java
@@ -0,0 +1,13 @@
+package cl.proyecto2.farmaceutica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaceuticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaceuticaApplication.class, args);
+	}
+
+}
---
+++ farmaceutica/src/test/java/cl/proyecto2/farmaceutica/FarmaceuticaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.proyecto2.farmaceutica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaceuticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
